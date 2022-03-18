4aad90e384e08b845a4a082376a902f8fea31c0b
Felix Bonillo <felixbdev@gmail.com>
2022-03-18 15:59:10 -0400

    Se anade el calendario

---
+++ public/js/agenda.js
@@ -0,0 +1,93 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    let formulario = document.querySelector("form");
+
+
+    var calendarEl = document.getElementById('agenda');
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+
+      initialView: 'dayGridMonth',
+      locale:"es",
+
+      headerToolbar: {
+          left: 'prev,nex today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek,listWeek'
+      },
+
+
+      dateClick:function(info){
+
+
+          $("#evento").modal("show");
+      },
+      eventClick:function(info){
+        var evento= info.event;
+        console.log(evento);
+
+        axios.post(baseURL+"/evento/editar/"+info.event.id).
+      then(
+        (respuesta) =>{
+          
+          formulario.id.value= respuesta.data.id;
+          formulario.title.value=respuesta.data.title;
+
+          formulario.descripcion.value=respuesta.data.descripcion;
+
+          formulario.start.value=respuesta.data.start;
+
+          formulario.end.value=respuesta.data.end;
+
+
+
+          $("#evento").modal("show");
+        }
+        ).catch(
+            error=>{
+              if(error.response){
+                console.log(error.response.data);
+              }
+            }
+        )
+      }
+
+    });
+    calendar.render();
+
+    document.getElementById("btnGuardar").addEventListener("click",function(){
+
+      enviarDatos("/evento/agregar");
+
+        });
+        document.getElementById("btnModificar").addEventListener("click",function(){
+          
+          enviarDatos("/evento/actualizar/"+formulario.id.value);
+
+        });
+        document.getElementById("btnEliminar").addEventListener("click",function(){
+          
+          enviarDatos("/evento/borrar/"+formulario.id.value);
+
+        });
+        
+        function enviarDatos(url){
+
+          const datos= new FormData(formulario);
+
+          const nuevaURL =  baseURL+url;
+
+          axios.post(nuevaURL, datos).
+          then(
+            (respuesta) =>{
+          
+              $("#evento").modal("hide");
+            }
+            ).catch(
+                error=>{if(error.response){ console.log(error.response.data);}
+                }
+              )
+
+        }
+        
+      });
