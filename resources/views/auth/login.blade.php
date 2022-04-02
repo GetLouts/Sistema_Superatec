9f41c55f0cbb1d9551925dff2fba1cf6d166f587
Felix Bonillo <felixbdev@gmail.com>
2022-04-01 20:18:43 -0400

    Modificacion del inicio de sesion (Estilos)

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,9 +3,9 @@
     Admin Login
 @endsection
 @section('content')
-    <div class="card bg-white caja p-1">
-        <div class="card-header justify-content-center">
-            <img src="{{ asset('img/logo.png') }}" alt="logo" width="300" class="">
+    <div class="card bg-white caja">
+        <div class="card-header justify-content-center logo">
+            <img src="{{ asset('img/logo.png') }}" alt="logo" class="logo">
         </div>
 
         <div class="card-body">
@@ -20,44 +20,41 @@
                         </ul>
                     </div>
                 @endif
-                <div class="form-group">
-                    <label for="email">Email</label>
-                    <input aria-describedby="emailHelpBlock" id="email" type="email"
-                        class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email"
-                        placeholder="Ingresar Email" tabindex="1"
-                        value="{{ Cookie::get('email') !== null ? Cookie::get('email') : old('email') }}" autofocus
-                        required>
-                    <div class="invalid-feedback">
-                        {{ $errors->first('email') }}
+                <div class="form-group ">
+                    <div class="correo ">
+                        <label for="email">Email</label>
+                        <input aria-describedby="emailHelpBlock" id="email" type="email"
+                            class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email"
+                            placeholder="Ingresar Email" tabindex="1"
+                            value="{{ Cookie::get('email') !== null ? Cookie::get('email') : old('email') }}" autofocus
+                            required>
+                        <div class="invalid-feedback">
+                            {{ $errors->first('email') }}
+                        </div>
                     </div>
-                </div>
-
-                <div class="form-group">
-                    <div class="d-block">
-                        <label for="password" class="control-label">Contraseña</label>
+                    <div class="contrasena mt-2">
+                        <div class="d-block">
+                            <label for="password" class="control-label">Contraseña</label>
+                        </div>
+                        <input aria-describedby="passwordHelpBlock" id="password" type="password"
+                            value="{{ Cookie::get('password') !== null ? Cookie::get('password') : null }}"
+                            placeholder="Ingresar Contraseña"
+                            class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password"
+                            tabindex="2" required>
+                        <div class="invalid-feedback">
+                            {{ $errors->first('password') }}
+                        </div>
+                        <div class="custom-control custom-checkbox">
+                            <input type="checkbox" name="remember" class="custom-control-input" tabindex="3" id="remember"
+                                {{ Cookie::get('remember') !== null ? 'checked' : '' }}>
+                            <label class="custom-control-label" for="remember">Recordar Contraseña</label>
+                        </div>
                     </div>
-                    <input aria-describedby="passwordHelpBlock" id="password" type="password"
-                        value="{{ Cookie::get('password') !== null ? Cookie::get('password') : null }}"
-                        placeholder="Ingresar Contraseña"
-                        class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password"
-                        tabindex="2" required>
-                    <div class="invalid-feedback">
-                        {{ $errors->first('password') }}
+                    <div class="inicio ">
+                        <button type="submit" class="btn btn-lg btn-block btn-success mt-2" tabindex="4" id="inicio">
+                            Iniciar Sesion
+                        </button>
                     </div>
-                </div>
-
-                <div class="form-group">
-                    <div class="custom-control custom-checkbox">
-                        <input type="checkbox" name="remember" class="custom-control-input" tabindex="3" id="remember"
-                            {{ Cookie::get('remember') !== null ? 'checked' : '' }}>
-                        <label class="custom-control-label" for="remember">Recordar Contraseña</label>
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <button type="submit" class="btn btn-lg btn-block btn-success" tabindex="4" id="inicio">
-                        Iniciar Sesion
-                    </button>
                 </div>
             </form>
         </div>
